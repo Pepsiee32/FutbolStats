8725b1789beab42af85b2a0445ec963f5c0a6d8a
Pepsiee32 <facundo.pezzimenti@gmail.com>
2026-01-11 16:24:06 -0300

    Solucion error vercel v2

--- web/next.config.ts
+++ web/next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // Excluir reset-password del pre-renderizado estático
-  experimental: {
-    missingSuspenseWithCSRBailout: false,
-  },
 };
 
 export default nextConfig;
